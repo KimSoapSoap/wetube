1b6dd74ef3e8a9b5568adde2bdb03474ae9b84c7
kimsoapsoap <zerfa@naver.com>
2022-03-25 00:47:23 +0900

    keep going

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,26 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
   res.render("join", { pateTitle: "Create Account" });
 export const postJoin = async (req, res) => {
   console.log(req.body);
-  const { email, name, username, password, location } = req.body;
+  const { email, name, username, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.status(400).render("Join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+
+  const alreadyExists = await User.exists({ $or: [{ username }, { email }] });
+  if (alreadyExists) {
+    console.log("hi", alreadyExists);
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "This email/username is already taken",
+    });
+  }
   try {
     await User.create({
       email,
@@ -15,12 +31,37 @@ export const postJoin = async (req, res) => {
     });
     return res.redirect("/login");
   } catch (error) {
-    console.log(error.MongoServerError);
-    return res.redirect("/join");
+    return res
+      .status(400)
+      .render("join", { pageTitle: "Join", errorMessage: error._message });
   }
 };
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  console.log(user.password);
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res
+      .status(400)
+      .render("login", { pageTitle, errorMessage: "Wrong password" });
+  }
+  console.log("user logged in!");
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -11,6 +13,18 @@ console.log("훈아 오늘도 화이팅");
 app.set("view engine", "pug");
 app.set("views", "./src/views");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+app.use(localsMiddleware);
+/*
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sesseions) => {
+    console.log(sesseions);
+    next();
+  });
+});
+*/
+
 app.use(logger);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
